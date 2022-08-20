af77771711422e1f19decc22fc1f9226278fbd55
4books <joonwoo9192@gmail.com>
2022-08-20 11:27:29 +0900

    알고리즘

---
+++ src/main/java/leetcode/string/ValidParentheses.java
@@ -0,0 +1,43 @@
+package leetcode.string;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        ValidParentheses s = new ValidParentheses();
+        boolean valid = s.isValid("()[]{}");
+        System.out.println("valid = " + valid);
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                //) ] }
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character pop = stack.pop();
+                if(c == ')' && pop == '('){
+                    continue;
+                }
+                if (c == ']' && pop == '[') {
+                    continue;
+                }
+                if (c == '}' && pop == '{') {
+                    continue;
+                }
+                //pop = (   c = }
+                return false;
+            }
+        }
+        //(
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+
+}

222d53df2493a4ecd43bfcfe18129f3e92f70513
naegwon <joonwoo9192@gmail.com>
2024-07-01 23:16:25 +0900

    알고리즘

---
+++ src/main/java/doit/arrayandlist/Q002.java
@@ -0,0 +1,15 @@
+package doit.arrayandlist;
+
+public class Q002 {
+    public static double solution(int n, int[] scores){
+
+        long sum = 0;
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, scores[i]);
+            sum += scores[i];
+        }
+
+        return sum * 100.0 / max / n;
+    }
+}
